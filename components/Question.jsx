79edd3d7034c0af3b16179f9b227e95f1aad4866
Kaori Kawakami <k.kawakami08@gmail.com>
2023-03-29 17:27:35 -0700

    style header image to overlap properly + responsive

---
+++ components/Question.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Question() {
+  return (
+    <div>
+        <h2>How many team members can I invite?</h2>
+        <p>You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan. </p>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import Head from "next/head"
+import Question from "@/components/Question"
+import Image from "next/image"
+import headerImage from '../public/images/illustration-woman-online-mobile.svg'
+import headerImageShadow from '../public/images/bg-pattern-mobile.svg'
 
 export default function Home() {
   return (
@@ -6,8 +10,34 @@ export default function Home() {
       <Head>
         <title>FAQ Accordian Card</title>
       </Head>
-      <main className="">
-        <h1 className="text-softRed text-5xl font-bold" >Whereas </h1>
+      <main className="flex flex-col items-center">
+        <div className="grid grid-cols-12 relative grid-rows-2">
+          <div className="col-start-3 col-span-8 row-span-2 row-start-1 ">
+            <Image 
+              src={headerImage}
+              alt="Header Image"
+              width={500}
+              height={500}
+            />
+          </div>
+          <div className="col-start-3 col-span-8 row-start-2 -z-10 ">
+            <Image 
+              src={headerImageShadow}
+              alt="Header Image Shadow"
+              width={500}
+              height={500}
+            />
+          </div>
+        </div>
+
+        <div>
+          <h1>FAQ</h1>
+          <Question />
+          <Question />
+          <Question />
+          <Question />
+          <Question />
+        </div>
       </main>
     </>
   )
